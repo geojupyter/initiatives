03b8492aa8fa48f0ff866a6674f8b364dd7ae071
Matt Fisher <mfisher87@gmail.com>
2026-05-26 15:40:54 -0600

    Add max_words validator to each section

--- .github/issue_validator.py
+++ .github/issue_validator.py
@@ -11,34 +11,40 @@ HEADING_REQUIREMENTS: list[HeadingRequirement] = [
     {
         "heading": "Problem statement",
         "min_words": 10,
+        "max_words": 100,
     },
     {
         "heading": "Who is impacted by this problem?",
         "min_words": 1,
+        "max_words": 100,
     },
     {
         "heading": "Proposed solution",
         "min_words": 10,
-        "max_words": 500,
+        "max_words": 200,
     },
     {
         "heading": "Proposed implementation",
         "min_words": 10,
-        "max_words": 500,
+        "max_words": 200,
     },
     {
         "heading": "How will this fit in the ecosystem?",
         "min_words": 10,
+        "max_words": 200,
     },
     {
         "heading": "How do we identify the right time to do this? (Is it now?)",
         "min_words": 10,
+        "max_words": 100,
     },
     {
         "heading": "Who is doing / will do the work?",
+        "max_words": 100,
     },
     {
         "heading": "Endorsements",
+        "max_words": 100,
     },
 ]
 FREEFORM_HEADINGS = [
